ba1609a6db9e1113fb07cb2ae0f50e95a8dc8565
Николай Сивцев <fasper@yandex.ru>
2020-03-06 17:49:22 +0300

    Update Math.php

--- src/Entity/Math.php
+++ src/Entity/Math.php
@@ -4,6 +4,13 @@ namespace nsivtsev\SimpleMathBundle\Entity;
 
 class Math implements MathInterface
 {
+    /**
+     * Math constructor.
+     */
+    public function __construct()
+    {
+    }
+
     public function solveFromString(string $problem)
     {
         return "OLOLO";
