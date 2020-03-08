f62ec38467a2018a1ea6077f47345bca69a147a9
Nikolay Sivtsev <fasper@yandex.ru>
2020-03-08 21:54:06 +0300

    tests

--- tests/Util/MathTest.php
+++ tests/Util/MathTest.php
@@ -7,11 +7,27 @@ use PHPUnit\Framework\TestCase;
 
 class MathTest extends TestCase
 {
-    public function testAdd()
+    public function testPrior()
     {
         $calculator = new Math();
         $result = $calculator->solve("2+2*2");
 
         $this->assertEquals(6, $result);
+    }
+
+    public function testBrackets()
+    {
+        $calculator = new Math();
+        $result = $calculator->solve("(2+2)*2");
+
+        $this->assertEquals(8, $result);
+    }
+
+    public function testZeroDivision()
+    {
+        $calculator = new Math();
+        $result = $calculator->solve("2*0");
+
+        $this->assertEquals(0, $result);
     }
 }
