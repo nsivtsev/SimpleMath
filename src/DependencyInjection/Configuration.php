8682ced136653255a4cea1a25cd294f3299a4b7e
Nikolay Sivtsev <fasper@yandex.ru>
2020-03-14 18:26:05 +0300

    wip

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -10,7 +10,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $treeBuilder->root('health_check');
+        $treeBuilder->root('math');
         return $treeBuilder;
     }
 }
