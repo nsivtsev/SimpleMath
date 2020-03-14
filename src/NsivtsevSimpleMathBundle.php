adfbb98225bd7bd637e13d61145fabdf6eba7e6a
Nikolay Sivtsev <fasper@yandex.ru>
2020-03-14 17:18:37 +0300

    wip

--- src/NsivtsevSimpleMathBundle.php
+++ src/NsivtsevSimpleMathBundle.php
@@ -2,8 +2,13 @@
 namespace nsivtsev\SimpleMathBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 class NsivtsevSimpleMathBundle extends Bundle
 {
-
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+        $container->registerForAutoconfiguration(Math::class)->addTag(Math::TAG);
+    }
 }
