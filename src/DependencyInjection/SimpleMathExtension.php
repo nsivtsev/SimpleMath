3bc959b531bfab3f768facb795b5daa473e1fa1c
Nikolay Sivtsev <fasper@yandex.ru>
2020-03-14 17:48:48 +0300

    wip

--- src/DependencyInjection/SimpleMathExtension.php
+++ src/DependencyInjection/SimpleMathExtension.php
@@ -14,10 +14,7 @@ class SimpleMathExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container)
     {
-        $configuration = new Configuration();
-        $this->processConfiguration($configuration, $configs);
-
-        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
-        $loader->load('services.yaml');
+        $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.xml');
     }
 }
