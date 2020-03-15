ea021af3970bdf749f05fdcc11488fc289499afe
Nikolay Sivtsev <fasper@yandex.ru>
2020-03-15 08:57:59 +0300

    tests

--- src/Service/Evaluator/Expressions/Division.php
+++ src/Service/Evaluator/Expressions/Division.php
@@ -1,6 +1,7 @@
 <?php
 namespace nsivtsev\SimpleMathBundle\Service\Evaluator\Expressions;
 
+use InvalidArgumentException;
 use nsivtsev\SimpleMathBundle\Service\Evaluator\Stack;
 
 class Division extends Operator {
@@ -10,6 +11,8 @@ class Division extends Operator {
     public function operate(Stack $stack) {
         $left = $stack->pop()->operate($stack);
         $right = $stack->pop()->operate($stack);
+        if ($left == 0)
+            throw new InvalidArgumentException('Division by zero');
         return $right / $left;
     }
 
